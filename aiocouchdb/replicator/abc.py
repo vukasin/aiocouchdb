d0611567afa0bcbf0fb21d8dc8b6a0893381e705
Alexander Shorin <kxepal@gmail.com>
2015-05-04 19:15:11 +0300

    Replication: verify peers
    To proceed replication, we need to ensure that both source and target
    peers are exists and available to handle further requests.

--- aiocouchdb/replicator/abc.py
+++ aiocouchdb/replicator/abc.py
@@ -8,6 +8,7 @@
 #
 
 import abc
+import asyncio
 
 
 class ISourcePeer(object, metaclass=abc.ABCMeta):
@@ -16,9 +17,58 @@ class ISourcePeer(object, metaclass=abc.ABCMeta):
     def __init__(self, peer_info):
         pass
 
+    @abc.abstractmethod
+    @asyncio.coroutine
+    def exists(self) -> bool:
+        """Checks if Database is exists and available for further queries.
+
+        :rtype: bool
+        """
+
+    @abc.abstractmethod
+    @asyncio.coroutine
+    def info(self) -> dict:
+        """Returns information about Database. This dict object MUST contains
+        the following fields:
+
+        - **instance_start_time** (`str`) - timestamp when the Database was
+          opened, expressed in microseconds since the epoch.
+
+        - **update_seq** - current database Sequence ID.
+
+        :rtype: dict
+        """
+
 
 class ITargetPeer(object, metaclass=abc.ABCMeta):
     """Target peer interface."""
 
     def __init__(self, peer_info):
         pass
+
+    @abc.abstractmethod
+    @asyncio.coroutine
+    def exists(self) -> bool:
+        """Checks if database is exists and available for further queries.
+
+        :rtype: bool
+        """
+
+    @abc.abstractmethod
+    @asyncio.coroutine
+    def info(self) -> dict:
+        """Returns information about Database. This dict object MUST contains
+        the following fields:
+
+        - **instance_start_time** (`str`) - timestamp when the Database was
+          opened, expressed in microseconds since the epoch.
+
+        - **update_seq** - current database Sequence ID.
+
+        :rtype: dict
+        """
+
+    @abc.abstractmethod
+    @asyncio.coroutine
+    def create(self):
+        """Creates target database."""
--- aiocouchdb/replicator/replication.py
+++ aiocouchdb/replicator/replication.py
@@ -10,6 +10,7 @@
 
 import asyncio
 
+from .abc import ISourcePeer, ITargetPeer
 from .records import ReplicationTask
 
 
@@ -32,4 +33,32 @@ class Replication(object):
     @asyncio.coroutine
     def start(self):
         """Starts a replication."""
+        # couch_replicator:do_init/1
+        # couch_replicator:init_state/1
+        rep_task, source, target = self.rep_task, self.source, self.target
+
+        # we'll need source and target info later
+        source_info, target_info = yield from self.verify_peers(
+            source, target, rep_task.create_target)
+
         raise NotImplementedError
+
+    @asyncio.coroutine
+    def verify_peers(self, source: ISourcePeer, target: ITargetPeer,
+                     create_target: bool=False) -> tuple:
+        """Verifies that source and target databases are exists and accessible.
+
+        If target is not exists (HTTP 404) it may be created in case when
+        :attr:`ReplicationTask.create_target` is set as ``True``.
+
+        Raises :exc:`aiocouchdb.error.HttpErrorException` exception depending
+        from the HTTP error of initial peers requests.
+        """
+        source_info = yield from source.info()
+
+        if not (yield from target.exists()):
+            if create_target:
+                yield from target.create()
+        target_info = yield from target.info()
+
+        return source_info, target_info
